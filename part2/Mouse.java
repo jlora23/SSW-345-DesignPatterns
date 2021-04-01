b3c4fca694406736d03b225067c53597f3a8c3e5
jlora23 <48076390+jlora23@users.noreply.github.com>
2021-04-01 13:31:11 -0400

    Finished Lab

---
+++ part2/Mouse.java
@@ -0,0 +1,10 @@
+package part2;
+
+public class Mouse implements ComputerPart {
+
+
+@Override
+public void accept(ComputerPartVisitor computerPartVisitor) { computerPartVisitor.visit(this);
+}
+}
+
